dd67273eb3bd7df4251bd308090fce845d252995
princeabdul90 <abuabdu99@gmail.com>
2022-04-19 10:47:12 +0800

    app theme, scaffold, card 1 added functionality added

---
+++ lib/home_page.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import '../components/components.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _selectedIndex = 0;
+
+  List<Widget> pages = <Widget>[
+    const Card1(),
+    //todo: replace with card 2
+    Container(color: Colors.red,),
+    //todo: replace with card 3
+    Container(color: Colors.lightBlueAccent,),
+  ];
+
+  void _onTappedItem(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('My Recipe App', style: Theme.of(context).textTheme.headline6,),
+      ),
+      body: pages[_selectedIndex],
+
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: _onTappedItem,
+        selectedItemColor: Theme.of(context).textSelectionTheme.selectionColor,
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.card_giftcard), label: 'card 1',),
+          BottomNavigationBarItem(icon: Icon(Icons.card_giftcard), label: 'card 2',),
+          BottomNavigationBarItem(icon: Icon(Icons.card_giftcard), label: 'card 3',),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/utils/recipe_theme.dart
@@ -0,0 +1,99 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class RecipeTheme {
+  static TextTheme lightThemeText = TextTheme(
+    bodyText1: GoogleFonts.openSans(
+      fontWeight: FontWeight.w700,
+      fontSize: 14,
+      color: Colors.black
+    ),
+    headline1: GoogleFonts.openSans(
+      fontWeight: FontWeight.bold,
+      fontSize: 32,
+      color: Colors.black,
+    ),
+    headline2: GoogleFonts.openSans(
+      fontWeight: FontWeight.w700,
+      fontSize: 21,
+      color: Colors.black,
+    ),
+    headline3: GoogleFonts.openSans(
+      fontWeight: FontWeight.w600,
+      fontSize: 16,
+      color: Colors.black,
+    ),
+    headline6: GoogleFonts.openSans(
+      fontWeight: FontWeight.w600,
+      fontSize: 20,
+      color: Colors.black,
+    ),
+  );
+  static TextTheme darkThemeText = TextTheme(
+    bodyText1: GoogleFonts.openSans(
+        fontWeight: FontWeight.w700,
+        fontSize: 14,
+        color: Colors.white
+    ),
+    headline1: GoogleFonts.openSans(
+      fontWeight: FontWeight.bold,
+      fontSize: 32,
+      color: Colors.white,
+    ),
+    headline2: GoogleFonts.openSans(
+      fontWeight: FontWeight.w700,
+      fontSize: 21,
+      color: Colors.white,
+    ),
+    headline3: GoogleFonts.openSans(
+      fontWeight: FontWeight.w600,
+      fontSize: 16,
+      color: Colors.white,
+    ),
+    headline6: GoogleFonts.openSans(
+      fontWeight: FontWeight.w600,
+      fontSize: 20,
+      color: Colors.white,
+    ),
+  );
+
+  static ThemeData light() {
+    return ThemeData(
+      brightness: Brightness.light,
+      textTheme: lightThemeText,
+      appBarTheme: const AppBarTheme(
+        foregroundColor: Colors.black,
+        backgroundColor: Colors.white,
+      ),
+      checkboxTheme: CheckboxThemeData(
+        fillColor: MaterialStateColor.resolveWith((states) => Colors.black),
+      ),
+      floatingActionButtonTheme: const FloatingActionButtonThemeData(
+        foregroundColor: Colors.white,
+        backgroundColor: Colors.black
+      ),
+      bottomNavigationBarTheme: const BottomNavigationBarThemeData(
+        selectedItemColor: Colors.green
+      ),
+    );
+  }
+
+  static ThemeData dark() {
+    return ThemeData(
+      brightness: Brightness.dark,
+      textTheme: darkThemeText,
+      appBarTheme:  const AppBarTheme(
+        foregroundColor: Colors.white,
+        backgroundColor: Colors.black,
+      ),
+
+      floatingActionButtonTheme: const FloatingActionButtonThemeData(
+          foregroundColor: Colors.white,
+          backgroundColor: Colors.green
+      ),
+      bottomNavigationBarTheme: const BottomNavigationBarThemeData(
+          selectedItemColor: Colors.green
+      ),
+    );
+  }
+}
