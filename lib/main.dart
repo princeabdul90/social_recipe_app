2af11289ce4665a3422836fd09d661fcdf51542b
princeabdul90 <abuabdu99@gmail.com>
2022-04-22 17:24:15 +0800

    theme added to main page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:recipe_app/utils/recipe_theme.dart';
 import 'home_page.dart';
 
 void main() {
@@ -11,12 +12,10 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    final theme = RecipeTheme.dark();
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-
-        primarySwatch: Colors.blue,
-      ),
+      title: 'Recipe App',
+      theme:theme,
       home: const HomePage(),
     );
   }
